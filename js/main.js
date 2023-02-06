d31fda24f57468ada7da2ceb122c572ad4537b82
Marvin <itsmrvinrsk@outlook.de>
2023-02-06 01:40:38 +0100

    + first draft of splide carousel for designs

--- js/main.js
+++ js/main.js
@@ -22,11 +22,11 @@ window.onload = e => {
     const animateCursor = (e, interacting) => {
         const x = e.clientX - cursor.offsetWidth / 2, y = e.clientY - cursor.offsetHeight / 2;
         const keyframes = {
-            transform: `translate(${x}px, ${y}px) scale(${interacting ? 2 : 1})`
+            transform: `translate(${x}px, ${y}px) scale(${interacting ? 1.7 : 1})`
         }
 
         cursor.animate(keyframes, {
-            duration: 50,
+            duration: 800,
             fill: 'forwards'
         });
 
@@ -59,5 +59,63 @@ window.onload = e => {
 
     window.onkeydown = e => {
         handleMouseHiding();
+    }
+
+
+    // Popups
+    document.querySelectorAll(".popup").forEach((popup) => {
+        const close = document.createElement("div");
+        close.classList.add("close");
+        close.addEventListener("click", () => {
+            togglePopup(popup.id);
+        });
+
+        popup.appendChild(close);
+    });
+
+    document.querySelectorAll("[data-toggle-popup]").forEach((toggle) => {
+        toggle.addEventListener("click", () => {
+            togglePopup(toggle.getAttribute("data-toggle-popup"));
+        });
+    });
+
+    if (document.querySelector('.popups')) {
+        document.querySelector('.popups').addEventListener('click', () => {
+            document.querySelectorAll('.popup').forEach(popup => {
+                popup.classList.remove('show');
+            });
+
+            document.querySelector('.popups').classList.remove('show');
+            document.body.classList.remove('popup-shown');
+        });
+    }
+
+    if (document.querySelector('.popup')) {
+        document.querySelectorAll('.popup').forEach(popup => {
+            popup.addEventListener('click', e => {
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+            });
+        });
+    }
+}
+
+function togglePopup(id) {
+    document.getElementById(id).classList.toggle("show");
+
+    if (document.getElementById(id).classList.contains("show")) {
+        document.body.classList.add('popup-shown');
+        document.querySelector('.popups').classList.add('show');
+    } else {
+        document.body.classList.remove('popup-shown');
+        document.querySelector('.popups').classList.remove('show');
+    }
+}
+
+function popupShown(id = null) {
+    if (id) {
+        return document.getElementById(id).classList.contains("show");
+    } else {
+        return document.querySelector('.popup.show') !== null;
     }
 }
